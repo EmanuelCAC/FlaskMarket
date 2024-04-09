9121f182ba8fa35a89bd827ec2b004d02fd30778
EmanuelCAC <emanuelc279@gmail.com>
2024-04-09 15:52:10 -0300

    restructuring the project

---
+++ market/__init__.py
@@ -0,0 +1,8 @@
+from flask_sqlalchemy import SQLAlchemy 
+from flask import Flask, render_template
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app)
+
+from market import routes
+from market import models

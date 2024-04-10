e38b285ebe7fe02352774472f2a8ea310f8d68ab
EmanuelCAC <emanuelc279@gmail.com>
2024-04-10 10:16:49 -0300

    setting the flask form for registration

--- market/__init__.py
+++ market/__init__.py
@@ -1,8 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy 
 from flask import Flask, render_template
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'aa91be99e853f0ab9e018fa5'
 db = SQLAlchemy(app)
 
 from market import routes
-from market import models

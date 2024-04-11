6cfa2c9ffc397229f587934c142257b96e4a31fb
EmanuelCAC <emanuelc279@gmail.com>
2024-04-11 10:17:06 -0300

    added login and user authentication

--- market/__init__.py
+++ market/__init__.py
@@ -1,9 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy 
-from flask import Flask, render_template
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = 'aa91be99e853f0ab9e018fa5'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from market import routes
